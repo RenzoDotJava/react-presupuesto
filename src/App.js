34cd4cce8eea00979f2481fe33a6d377dace821a
Renzo Bedriñana Orozco <71391592+RenzoDotJava@users.noreply.github.com>
2021-06-11 01:03:23 -0500

    presupuesto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //definimos los states
+    const [presupuesto, guardarPresupuesto] = useState(0);
+    const [restante, guardarRestante] = useState(0);
+    const [mostrarPregunta, actualizarPregunta] = useState(true);
+    const [gastos, guardarGastos] = useState([]);
+    const [gasto, guardarGasto] = useState({});
+    const [crearGasto, guardarCrearGasto] = useState(false);
+
+    //useEffect que actualiza el restante
+
+    useEffect(() => {
+        if (crearGasto) {
+            //Agregar gasto
+            guardarGastos([...gastos, gasto]);
+            //Restamos el presupuesto
+            const presupuestoRestante = restante - gasto.cantidad;
+            guardarRestante(presupuestoRestante);
+        }
+
+        //Resetear a false
+        guardarCrearGasto(false);
+    }, [gasto, crearGasto, gastos, restante]);
+
+    return (
+        <div className="container">
+            <header>
+                <h1>Gasto Semanal</h1>
+                <div className="contenido-principal contenido">
+                    {mostrarPregunta ? (
+                        <Pregunta
+                            guardarPresupuesto={guardarPresupuesto}
+                            guardarRestante={guardarRestante}
+                            actualizarPregunta={actualizarPregunta}
+                        />
+                    ) : (
+                        <div className="row">
+                            <div className="one-half column">
+                                <Formulario
+                                    guardarGasto={guardarGasto}
+                                    guardarCrearGasto={guardarCrearGasto}
+                                />
+                            </div>
+                            <div className="one-half column">
+                                <Listado gastos={gastos} />
+
+                                <ControlPresupuesto
+                                    presupuesto={presupuesto}
+                                    restante={restante}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
